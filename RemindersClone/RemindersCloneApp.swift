24ce73855260496da6a8152586c5cb02f3b717d4
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-10 10:42:54 -0500

    reminders clone initial commit

---
+++ RemindersClone/RemindersCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersCloneApp.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
