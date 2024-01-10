c37a8be20cbf891f63894c649517ef3cfe483631
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-10 11:34:26 -0500

    add data folder and core data provider

---
+++ RemindersClone/Data/CoreDataProvider.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataProvider.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 1/10/24.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataProvider {
+    
+    static let shared = CoreDataProvider()
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "ReminderModel")
+        persistentContainer.loadPersistentStores { descrtiption, error in
+            if let error {
+                fatalError("Error initializing Reminders Model: \(error.localizedDescription)")
+            }
+        }
+    }
+}
