d9379ea3f959007c1a3fbde445ccd08e6e58a6d2
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-15 12:39:34 -0500

    add notification folder and notification manager, add user data struct

---
+++ RemindersClone/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 1/15/24.
+//
+
+import Foundation
+
+struct UserData {
+    let title: String?
+    let body: String?
+    let date: Date?
+    let time: Date?
+}
---
+++ RemindersClone/Notifications/NotificationManager.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationManager.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 1/15/24.
+//
+
+import Foundation
+
+class NotificationManager {
+    static func scheduleNotification(userData: UserData) {
+        
+    }
+}
