d46c995889418f85726b060e5fad413d655992aa
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-15 12:45:31 -0500

    update notification manager with user notification and trigger

--- RemindersClone/Notifications/NotificationManager.swift
+++ RemindersClone/Notifications/NotificationManager.swift
@@ -6,9 +6,25 @@
 //
 
 import Foundation
+import UserNotifications
 
 class NotificationManager {
     static func scheduleNotification(userData: UserData) {
         
+        let content = UNMutableNotificationContent()
+        content.title = userData.title ?? ""
+        content.body = userData.body ?? "" // Reminder notes
+        
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: userData.date ?? Date())
+        
+        if let reminderTime = userData.time {
+            let reminderTimeDateComponents = reminderTime.dateComponents
+            dateComponents.hour = reminderTimeDateComponents.hour
+            dateComponents.minute = reminderTimeDateComponents.minute
+        }
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let request = UNNotificationRequest(identifier: "Reminder Notification", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
     }
 }
