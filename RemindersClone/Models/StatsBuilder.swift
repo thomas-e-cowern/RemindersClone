dfa61965bf0176fcb5faa059605f94d2bc356804
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-15 08:01:56 -0500

    add functions to calcualte stats to stats builder

--- RemindersClone/Models/StatsBuilder.swift
+++ RemindersClone/Models/StatsBuilder.swift
@@ -20,6 +20,38 @@ struct StatsBuilder {
     func build(myListResults: FetchedResults<MyList>) -> ReminderStatsValues {
         
         let remindersArray = myListResults.map { $0.remindersArray }.reduce([], +)
+        
+        let allCount = calculateAllCount(reminders: remindersArray)
+        let completedCount = calculateCompletedCount(reminders: remindersArray)
+        let todayCount = calculateTodayCount(reminders: remindersArray)
+        let scheduledCount = calculateScheduledCount(reminders: remindersArray)
+        
+        return ReminderStatsValues(todayCount: todayCount, scheduledCount: scheduledCount, allCount: allCount, completedCount: completedCount)
     }
     
+    private func calculateAllCount(reminders: [Reminder]) -> Int {
+        return reminders.reduce(0) { result, reminder in
+            return !reminder.isCompleted ? result + 1 : result
+        }
+    }
+    
+    private func calculateCompletedCount(reminders: [Reminder]) -> Int {
+        return reminders.reduce(0) { result, reminder in
+            return reminder.isCompleted ? result + 1 : result
+        }
+    }
+    
+    private func calculateTodayCount(reminders: [Reminder]) -> Int {
+        return reminders.reduce(0) { result, reminder in
+            let isToday = reminder.reminderDate?.isToday ?? false
+            return isToday ? result + 1 : result
+        }
+    }
+    
+    private func calculateScheduledCount(reminders: [Reminder]) -> Int {
+        return reminders.reduce(0) { result, reminder in
+            let isScheduled = (reminder.reminderDate != nil || reminder.reminderTime != nil) && !reminder.isCompleted
+            return isScheduled ? result + 1 : result
+        }
+    }
 }
